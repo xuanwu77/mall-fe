bdfc5beca5ef110d6fa5cff90d760b65c2ba7b04
xuanwu77 <xwun_n@163.com>
2017-07-13 20:52:48 +0800

    initial

---
+++ src/page/common/index.js
@@ -0,0 +1,11 @@
+/*
+* @Author: lenovo
+* @Date:   2017-07-12 13:11:22
+* @Last Modified by:   lenovo
+* @Last Modified time: 2017-07-12 13:12:04
+*/
+
+
+'use strict';
+
+console.log('i am global');
---
+++ src/page/index/index.js
@@ -0,0 +1,10 @@
+/*
+* @Author: lenovo
+* @Date:   2017-07-11 21:19:11
+* @Last Modified by:   lenovo
+* @Last Modified time: 2017-07-12 13:37:47
+*/
+
+'use strict';
+require('./index.css');
+require('../module.js');
